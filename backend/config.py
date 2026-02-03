144ba32db32d9bc05bbfcefefa0c0b1979262b9f
Zaid Shaikh <skzaid00000@gmail.com>
2026-02-04 01:27:23 +0530

    18

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,10 @@
-# backend/config.py
 import os
 from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_ANON_KEY")
+SUPABASE_SERVICE_KEY = os.getenv("SUPABASE_SERVICE_KEY")
 
 if not SUPABASE_URL or not SUPABASE_SERVICE_KEY:
-    raise Exception("Supabase environment variables not set!")
+    raise Exception("Supabase env vars missing")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
